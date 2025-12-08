0609bd54f219a59d828c60c9573df3cb78aafd98
pbcosina <lilyphoebegrace.cosina@cit.edu>
2025-12-08 18:18:43 +0800

    dashboard: updated dashboard interface and improved hover effects throughout cards except settings

--- fairshare1/src/main/java/com/example/mzp/fairshare1/repositories/ExpenseRepository.java
+++ fairshare1/src/main/java/com/example/mzp/fairshare1/repositories/ExpenseRepository.java
@@ -3,15 +3,15 @@ package com.example.mzp.fairshare1.repositories;
 import com.example.mzp.fairshare1.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findByGroupId(Long groupId);
-
     @org.springframework.data.jpa.repository.Query("SELECT e FROM Expense e WHERE e.group.id = :groupId AND (e.isSplit = true OR e.paidBy.id = :userId)")
-    List<Expense> findByGroupIdAndUserIdOrSplit(@org.springframework.data.param.Param("groupId") Long groupId,
-            @org.springframework.data.param.Param("userId") Long userId);
+    List<Expense> findByGroupIdAndUserIdOrSplit(@Param("groupId") Long groupId,
+            @Param("userId") Long userId);
 
     @Modifying
     @Transactional
